219c484d9ebf1d64bee6fc6769f8d32006b4b0aa
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-09-20 16:32:52 +0300

    hello

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,6 +1,6 @@
 from .views import HomeView, AboutView
 from django.urls import path
-
+# hello
 urlpatterns = [
 path('', HomeView.as_view(), name='home'),
 path('about', AboutView.as_view(), name='about'),
