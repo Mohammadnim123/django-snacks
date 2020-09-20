561be667d9ddc78c90fcd178d4f5aa26d7e3ee3f
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-09-20 17:25:41 +0300

    add another app

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snacks.apps.SnacksConfig'
+    'snacks.apps.SnacksConfig',
+    'coffee.apps.CoffeeConfig'
 ]
 
 MIDDLEWARE = [
